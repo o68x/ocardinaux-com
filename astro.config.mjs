a8f9b786ea55e6d84df5e1f8f54e065e628275fd
Olivier Cardinaux <ocardinaux@gmail.com>
2024-11-10 09:36:58 +0100

    fix: use lightningcss because stephanie eckles likes it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   publicDir: 'static',
   site: 'https://ocardinaux.com',
+  vite: {
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        targets: browserslistToTargets(browserslist('defaults'))
+      }
+    }
+  }
 });
