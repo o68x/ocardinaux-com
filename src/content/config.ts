b7a495ee8f46fabfcbf079d5761e0a2b66b7a90d
Olivier Cardinaux <ocardinaux@gmail.com>
2024-11-15 18:13:09 +0100

    feat!: screenshots is JSON collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from 'astro/loaders';
+import { glob, file } from 'astro/loaders';
 
 const sectionsCollection = defineCollection({
   loader: glob({pattern: "**\/[^_]*.mdoc", base: 'content/sections'}),
@@ -11,8 +11,20 @@ const sectionsCollection = defineCollection({
     ]).default("section"),
     draft: z.boolean().default(false),
   })
+});
+
+const screenshotsCollection = defineCollection({
+  loader: file("content/screenshots/screenshots.json"),
+  schema: ({ image }) =>z.object({
+    id: z.string(),
+    src: image(),
+    url: z.string(),
+    alt: z.string().optional(),
+    backside: z.string(),
+  })
 })
 
 export const collections = {
 	sections: sectionsCollection,
+  screenshots: screenshotsCollection,
 };
